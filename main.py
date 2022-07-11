8231e871531deb12c4fdc4305e3920d9e61af1fe
Miguel Barrios <mbarrios@enthec.com>
2022-07-11 18:08:10 +0200

    Refactor

--- main.py
+++ main.py
@@ -5,5 +5,3 @@ if __name__ == '__main__':
     domain = 'santander.com'
     search_img = asyncio.run(request_session(domain))
     print(search_img)
-
-# Comando:  python3 -m uvicorn main:app --reload /multihilo para analizar link en paralelo
